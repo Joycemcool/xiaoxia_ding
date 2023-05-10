9cbd4c9e215de95e9aad4a61bb5d0d1acd049bf4
Joycemcool <joyceding007@gmail.com>
2023-05-10 05:44:03 -0300

    Index and Project Card Display

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
-/*!
-* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// const left_xia = document.querySelector('.left_xia');
+// const right_xia = document.querySelector('.right_xia');
+
+// left_xia.addEventListener('mouseover', () => {
+//     right_xia.style.left = '0'; /* Move Element 2 to the left on hover over Element 1 */
+// });
+
+// left_xia.addEventListener('mouseout', () => {
+//     right_xia.style.left = '50px'; /* Move Element 2 back to its original position on mouseout of Element 1 */
+// });
+
+// var left_xia = document.getElementById("left_xia");
+// var right_xia = document.getElementById("right_xia");
+
+var left_xia = document.getElementsByClassName("left_xia");
+var right_xia = document.getElementsByClassName("right_xia");
+
+element1.addEventListener("mousemove", exchangePositions);
+element2.addEventListener("mousemove", exchangePositions);
+
+function exchangePositions(event) {
+
+    var temp = left_xia.style.left;
+    left_xia.style.left = right_xia.style.left;
+    right_xia.style.left = temp;
+  }
+  
